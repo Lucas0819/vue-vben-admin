184b7bac4aebd4131c6db2496fea566ce5fce5c5
Lucas <liuchegnzhao>
2023-05-14 16:33:35 +0800

    feat: 文件上传接口对应

---
+++ src/api/sys/file.ts
@@ -0,0 +1,35 @@
+import { UploadApiResult } from './model/uploadModel';
+import { defHttp } from '/@/utils/http/axios';
+import { UploadFileParams } from '/#/axios';
+import { useGlobSetting } from '/@/hooks/setting';
+import { AxiosProgressEvent } from 'axios';
+
+enum Api {
+  UploadFile = '/file/upload',
+  DownloadFile = '/file/download/{fileName}',
+}
+
+const globSetting = useGlobSetting();
+const urlPrefix = globSetting.upmsUrlPrefix;
+
+/**
+ * @description: Upload interface
+ */
+export function uploadApi(
+  params: UploadFileParams,
+  onUploadProgress: (progressEvent: AxiosProgressEvent) => void,
+) {
+  return defHttp.uploadFile<UploadApiResult>(
+    {
+      url: Api.UploadFile,
+      onUploadProgress,
+    },
+    {
+      ...params,
+      urlPrefix,
+    },
+  );
+}
+
+export const downloadFileUrl = (fileName: string): string =>
+  defHttp.getUrl({ url: Api.DownloadFile.replace('{fileName}', fileName) }, { urlPrefix });
