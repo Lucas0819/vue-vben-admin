98faddbf33b3a980696bc99e4fe58c4cf444839e
Lucas <liuchegnzhao>
2023-05-15 10:02:45 +0800

    ci: docker命令调整

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-docker login 8.146.200.64:5000 -u doupiao -p doupiao@2023
-
 APP='8.146.200.64:5000/dp-master-ui-test:1.0.0'
 
 npm run build
@@ -8,8 +6,10 @@ docker pull hub.c.163.com/library/nginx
 
 docker build -t $APP .
 
+docker login 8.146.200.64:5000 -u doupiao -p doupiao@2023
+
 docker push $APP
 
-docker rmi $APP
-
 docker logout 8.146.200.64:5000
+
+docker rmi $APP
